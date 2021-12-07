49d47335f35f4b48754a0f021bce204ac9f28ff2
Tuan Vo Dinh <vdt12319999@gmail.com>
2021-12-07 10:19:38 +0700

    Add modules in App module

--- blog-app/src/app/app.module.ts
+++ blog-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,10 +16,12 @@ import { EditorModule } from './modules/editor/editor.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AuthModule,
     HomeModule,
     ProfileModule,
