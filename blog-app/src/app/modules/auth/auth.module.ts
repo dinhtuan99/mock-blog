5d9f88d7833b38e1e2c05d83ba197989989f4e43
Tuan Vo Dinh <vdt12319999@gmail.com>
2021-12-07 13:14:21 +0700

    Add Header, Login, Fix UserService

--- blog-app/src/app/modules/auth/auth.module.ts
+++ blog-app/src/app/modules/auth/auth.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
-
-
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AuthComponent
+    AuthComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
