631fa30384b9aea34208eac13d7f1ce848ec1dd0
Tuan Vo Dinh <vdt12319999@gmail.com>
2021-12-07 13:17:08 +0700

    Add header Fix userSerivce

--- blog-app/src/app/services/user.service.ts
+++ blog-app/src/app/services/user.service.ts
@@ -22,6 +22,10 @@ export class UserService {
         return this.currentUserSubject.value;
     }
 
+    public currentUser(): Observable<IUser> {
+        return this.currentUserSubject;
+    }
+
     public setCurrentUser(user: IUser) {
         this.currentUserSubject.next(user);
     }
