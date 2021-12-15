ce9f77454b1a3ca2d6a169591d7b76a52bfa1581
manhns15 <manhns1511@gmail.com>
2021-12-15 16:25:29 +0700

    update UI

---
+++ .history/blog-app/src/app/modules/editor/new-article/new-article.component_20211215161652.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Article, IArticleCreate } from 'src/app/models/articles.model';
+import { ArticlesService } from 'src/app/services/articles.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-new-article',
+  templateUrl: './new-article.component.html',
+  styleUrls: ['./new-article.component.css']
+})
+export class NewArticleComponent implements OnInit {
+  formArt!: FormGroup;
+  articles!: IArticleCreate;
+  tags: string[] = [];
+  isSubmit: boolean = false;
+  constructor(private articleService: ArticlesService,
+    private formBuilder: FormBuilder,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.formArt = this.formBuilder.group({
+      title: '',
+      description: '',
+      body: '',
+      tagForm: ''
+    })
+  }
+  get iF() {
+    return this.formArt.controls;
+  }
+  onSubmit() {
+    this.isSubmit = true;
+    this.articles = {
+      article: {
+        title: this.iF.title.value,
+        description: this.iF.description.value,
+        body: this.iF.body.value,
+        tagList: this.tags
+      }
+    }
+    this.articleService.createArticle(this.articles).subscribe(res => {
+
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+
+      Toast.fire({
+        icon: 'success',
+        title: 'Add article in successfully'
+      })
+
+      this.router.navigateByUrl('/article/' + res.article.slug)
+    })
+  }
+  onAddTag() {
+
+    if (this.iF.tagForm.value) {
+      if (this.tags.indexOf(this.iF.tagForm.value) < 0) {
+        this.tags.push(this.iF.tagForm.value)
+      }
+    }
+    this.iF.tagForm.setValue('')
+  }
+  removeTag(tagName: string) {
+    this.tags = this.tags.filter(tag =>
+      tag !== tagName
+
+    )
+    console.log(this.tags);
+
+  }
+
+}
